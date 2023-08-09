955421f27da7b67c55c771bc44656cd45d3e08c5
Artem Bykov <bykovaa94@mail.ru>
2023-08-09 12:19:41 +0300

    webpack

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'development')
+    })
+  ]
+};
